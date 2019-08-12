81f446294247ebb7983615f16761628de82551d3
cpark99 <cpwebdeveloper99@gmail.com>
2019-08-12 10:59:25 -0700

    updated breakpoints, syntax

--- src/ScheduleCall/ScheduleCall.js
+++ src/ScheduleCall/ScheduleCall.js
@@ -10,7 +10,7 @@ export default class ScheduleCall extends Component {
           <p>Talk to a certified financial services professional for FREE!</p>
         </div>
         <div className="schedule-call-calendar-container calendar-container">
-          <div class="calendly-inline-widget" data-url="https://calendly.com/cpwebdeveloper99/30min" id="schedule-call-calendar-widget"></div>
+          <div className="calendly-inline-widget" data-url="https://calendly.com/cpwebdeveloper99/30min" id="schedule-call-calendar-widget"></div>
         </div>
       </section>
     )
