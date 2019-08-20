aeb9f53513ac6b6fc3a0c91c4864c9791ce208fe
Chris Park <cpwebdeveloper99@gmail.com>
2019-08-19 19:42:03 -0700

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://lit-plateau-20514.herokuapp.com/',
+  API_ENDPOINT: 'https://lit-plateau-20514.herokuapp.com',
   TOKEN_KEY: 'friendly-financial-client-auth-token',
 }
