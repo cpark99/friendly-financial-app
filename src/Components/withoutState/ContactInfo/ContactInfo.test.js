dfe4aed9ad44f0c91358f89bbc81aabdf115446a
Chris Park <cpwebdeveloper99@gmail.com>
2019-09-05 16:00:32 -0700

    added test

---
+++ src/Components/withoutState/ContactInfo/ContactInfo.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ContactInfo from './ContactInfo';
+
+window.scrollTo = jest.fn();
+
+it('renders without crashing', () => {
+  window.scrollTo.mockClear();
+
+  const div = document.createElement('div');
+  ReactDOM.render(
+      <ContactInfo />,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
