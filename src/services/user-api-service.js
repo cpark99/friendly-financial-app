eae38a1ad14b1936b25e08cebce6befd8f938b05
Chris Park <cpwebdeveloper99@gmail.com>
2019-09-06 10:35:27 -0700

    cleaned code

--- src/services/user-api-service.js
+++ src/services/user-api-service.js
@@ -2,13 +2,6 @@ import config from "../config";
 import TokenService from "./token-service";
 
 const UserApiService = {
-  getUsers() {
-    return fetch(`${config.API_ENDPOINT}/users`, {
-      headers: {}
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-    );
-  },
   getUser(userId) {
     return fetch(`${config.API_ENDPOINT}/users/${userId}`, {
       headers: {
